47981ffeba5f6f3f30fe299371eb14af2876b7c6
PC <your.email@example.com>
2025-05-01 19:20:10 +0900

    .

---
+++ build/andino_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yeokim/Moppy/src/simulation/ros2_ws/src/andino_webots/submodules/andino/andino_description
+# Build directory: /home/yeokim/Moppy/build/andino_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/andino_firmware/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yeokim/Moppy/src/simulation/ros2_ws/src/andino_webots/submodules/andino/andino_firmware
+# Build directory: /home/yeokim/Moppy/build/andino_firmware
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
