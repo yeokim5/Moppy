f20556eb5ef70367ee45492d4a029a4e108eb5a0
PC <your.email@example.com>
2025-05-01 19:22:37 +0900

    .

--- build/andino_firmware/CMakeFiles/andino_firmware_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for andino_firmware_uninstall.
-# This may be replaced when dependencies are built.
--- build/node_remover_plugin/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/yeokim/Moppy/install/node_remover_plugin'
